ba203b204576879bd09bf32580ffd709643ccb1f
Bujdea Bogdan <bujdeabogdan@gmail.com>
2011-05-17 19:54:56 +0300

    First Commit

---
+++ STL.cpp
@@ -0,0 +1,8 @@
+#include "STL.h"
+
+
+int main(int argc, char* argv[])
+{
+	map<string, int> months;
+
+}
---
+++ STL.h
@@ -0,0 +1,6 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
