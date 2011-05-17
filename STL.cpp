3fd4707c598b274a9b164b6157494a0a8844cae2
Bujdea Bogdan <bujdeabogdan@gmail.com>
2011-05-17 20:06:23 +0300

    Map example with a phone book(name, phone number)

--- STL.cpp
+++ STL.cpp
@@ -3,6 +3,24 @@
 
 int main(int argc, char* argv[])
 {
-	map<string, int> months;
+	map<string, string> PhoneBook;
+	int i = 0, n = 0;
+	string name, number;
+	cout << "\nNumarul de contacte:"; cin>>n;
+	cin.ignore();
+	for(i = 0; i < n; i++)
+	{
+		cout << "\nPerson " << i + 1 << " name:"; getline(cin, name);
+		cout << "\nPerson " << i + 1 << " phone number:"; getline(cin, number);
+		PhoneBook[name] = number;
+	}
+	map<string, string>::iterator it = PhoneBook.begin();
+	for(it = PhoneBook.begin(), i = 0; it != PhoneBook.end(); it++, i++)
+	{
+		cout << "\nPerson " << i + 1 <<
+			" name: " << it->first;
+		cout << "\nPerson " << i + 1 << " phone number: " << it->second<<endl;
+	}
 
+	return 0;
 }
